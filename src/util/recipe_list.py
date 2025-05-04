ae10f820459368daa06dedfa3d6efe141b9d65d0
Javin Mendiratta <javin.mendiratta@gmail.com>
2025-05-04 17:09:37 -0400

    Added the front end in the app folder, made adjustments to retrieval and util to allow for proper interaction between server and client.

--- src/util/recipe_list.py
+++ src/util/recipe_list.py
@@ -13,6 +13,33 @@ class RecipeList():
     async def updateRecipes(self, new_recipes: list[any]) -> None:
         '''updates the recipe list with a new recipe list (updated for ascyncio)'''
         self.recipes = await new_recipes.to_list()
+
+    def to_JSON(self) -> object:
+        recipe_obj_list = []
+        for recipe in self.recipes:
+
+            recipe_ing_list = []
+            for ingredient in recipe["Ingredients"]:
+                recipe_ing_list.append({
+                    "ingredient": ingredient["IngredientName"],
+                    "quantity": str(ingredient["Quantity"])
+                })
+
+            recipe_inst_list = []
+            for instruction in recipe["RecipeInstructions"]:
+                recipe_inst_list.append(instruction)
+
+            recipe_obj_list.append({
+                "name": recipe["Name"],
+                "calories": recipe["Calories"],
+                "desc": recipe["Description"],
+                "ingredients": recipe_ing_list,
+                "instructions": recipe_inst_list
+            })
+
+        return {
+            "recipes": recipe_obj_list
+        }
          
     def __str__(self):
         '''creates print string for the Recipe List'''
